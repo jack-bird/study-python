b6ca2fbdded658d23ed49559921d881cf11fe8ef
jack-bird <1804823423@qq.com>
2017-06-09 09:48:18 +0800

    linux

--- model.py
+++ model.py
@@ -19,16 +19,16 @@ import sys
 # if __name__=='__main__':
 #     test()
 
-def _private_1(name):
-    return 'Hello,%s'%name
+#def _private_1(name):
+#    return 'Hello,%s'%name
 
-def _private_2(name):
-    return 'Hi,%s'%name
+#def _private_2(name):
+#    return 'Hi,%s'%name
 
-def greeting(name):
-    if len(name)>3:
-        return _private_1(name)
-    else:
-        return _private_2(name)
+#def greeting(name):
+#    if len(name)>3:
+#        return _private_1(name)
+#    else:
+#        return _private_2(name)
 
 
